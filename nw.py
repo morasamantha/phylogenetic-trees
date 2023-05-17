19fe05c9fe4f61321d18d68f577386fa97423939
Sam Mora <morasamantha@ciencias.unam.mx>
2023-05-17 12:47:30 -0600

    Creo q esta es la buena :-)

--- nw.py
+++ nw.py
@@ -47,40 +47,39 @@ def needleman_wunsch(str1, str2, match, mismatch, indel):
         i = queue.pop()
         match i:
             case "d":
-                c1 += a.popleft()
-                c2 += b.popleft()
+                c1 += a.pop()
+                c2 += b.pop()
                 # Como es diagonal popeamos lstr2 que no nos sirven
                 for j in range(lstr2):
                     if queue: queue.pop()
-                print("\n***","d", c1, c2, "***", sep="\n")
             case "i":
                 c1 += "-"
-                c2 += b.popleft()
+                c2 += b.pop()
                 # No popeamos nada
-                print("\n***","i", c1, c2, "***", sep="\n")
             case "a":
-                c1 += a.popleft()
+                c1 += a.pop()
                 c2 += "-"
                 # Popeamos sólo lstr2-1
                 for j in range(lstr2-1):
                     if queue: queue.pop()
-                print("\n***","i", c1, c2, "***", sep="\n")
 
     #Existe el caso de que alguno tenga algo aún
     while len(a) > 0:
-        print("Quedó algo A")
-        c1 += a.popleft()
-        c2 = "-" + c2
+        c1 += a.pop()
+        c2 += "-"
 
     while len(b) > 0:
-        print("Quedó algo B")
-        c2 += b.popleft()
-        c1 = "-" + c1
+        c2 += b.pop()
+        c1 += "-"
+    #originalmente estaban al revés
+    c1 = c1 [::-1]
+    c2 = c2 [::-1]
     #Este print lo borraremos después 
     print(c1,c2, sep="\n")
     return(c1,c2)
 
 # sólo pa pruebas xc
-# La que ya pasa
 needleman_wunsch("agta", "gagta", 1,-1,-1)
-
+needleman_wunsch("agta", "agata", 1,-1,-1)
+needleman_wunsch("agta", "gagt", 1,-1,-1)
+needleman_wunsch("agta", "gatt", 1,-1,-1)

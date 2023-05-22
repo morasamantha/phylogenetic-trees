c387df84f1b07bbba552800ae221989e0d93ea1a
Sam Mora <morasamantha@ciencias.unam.mx>
2023-05-22 13:34:35 -0600

    Estructura de carpetas

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from tabulate import tabulate
+
+# Leer archivos
+# Alinear secuencias
+# Matriz de distancia 1
+# Matriz de distancia 2
+# Matriz de distancia 3
+# Matriz de distancia 4
+# Árbol con NJ
+# Árbol con UPGMA
+# ...
+# Críterio de parsimonía

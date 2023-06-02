bdb62c7a9ceb4a4907ceca19451efdfbb3e2e33b
Sam Mora <morasamantha@ciencias.unam.mx>
2023-06-02 00:11:05 -0600

    Avances impresión gráfica jdlsakd:-(

--- src/trees.py
+++ src/trees.py
@@ -1,6 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import networkx as nx
+import matplotlib.pyplot as plt
+
 
 def nj(dmatrix, g, taxas):
     n = len(taxas)
@@ -35,14 +36,15 @@ def nj(dmatrix, g, taxas):
         dist_update = np.zeros((n-1,n-1), dtype = float)
 
         nuevas_distancias = []
-
-        for v in range(len(taxa)-1):
+        for v in range(n):
             if v == i:
                 pass
             elif v == j:
-                pass:
+                pass
             else :
                 nuevas_distancias.append(0.5 * (dmatrix[i][v] + dmatrix[v][j] - dmatrix[i][j]) )
+
+        nuevas_distancias.append(0)
 
         dmatrix1 = np.delete(dmatrix, j, 0)
         dmatrix1 = np.delete(dmatrix1, j, 1)
@@ -55,6 +57,8 @@ def nj(dmatrix, g, taxas):
             for fila in range(n_nueva-1):
                 dist_update[columna][fila] = dmatrix1[columna][fila]
         
+        print(dist_update)
+        print(nuevas_distancias)
         dist_update[:,-1] = nuevas_distancias
         dist_update[-1,:] = nuevas_distancias
 
@@ -66,15 +70,29 @@ def nj(dmatrix, g, taxas):
         if(g.has_edge(0, taxa_i)) : g.remove_edge(0, taxa_i)
         if(g.has_edge(0, taxa_j)) : g.remove_edge(0, taxa_j)
         # Los agregamos a V con todo y sus branch length 
-        g.add_edge(0, taxa_i, Vi)
-        g.add_edge(0, taxa_j, Vj)
+        g.add_edge(nuevo_vertice, taxa_i, weight=Vi)
+        g.add_edge(nuevo_vertice, taxa_j, weight=Vj)
 
         # Seguimos calculandoooooo....
         nj(dist_update, g, taxas)
     # Paso 4
     elif n == 2:
         # Unimos los dos que quedan, by a branch length d(ti,tj)
-        return g
+        g.add_edge(taxas[0], taxas[1], weight=dmatrix[0,1])
+
+        pos = nx.spring_layout(g, seed=7)  # positions for all nodes - seed for reproducibility
+        # nodes
+        nx.draw_networkx_nodes(g, pos, node_size=700)
+
+        # edges
+        nx.draw_networkx_edges(g, pos, g.edges, width=6)
+        # node labels
+        nx.draw_networkx_labels(g, pos, font_size=20, font_family="sans-serif")
+        # edge weight labels
+        edge_labels = nx.get_edge_attributes(g, "weight")
+        nx.draw_networkx_edge_labels(g, pos, edge_labels)
+        plt.savefig('plotgraph.png', dpi=300, bbox_inches='tight')
+        #return g De hecho no la quiero devolver xp Vamos a ver si puede crear un png desde acá....
     else:
         raise Exception("Algo salió mal. :-) <3")
 
@@ -87,7 +105,7 @@ t = ['a', 'b', 'c', 'd']
 # Formando la estreeellaaaa.
 G = nx.Graph()
 G.add_node(0)
-for taxa in t:
+for taxa in range(len(t)):
     G.add_node(t[taxa])
     G.add_edge(0, t[taxa])
     

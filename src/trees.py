eb3af49b3629a84dbfac04194aefb3154169f80e
Sam Mora <morasamantha@ciencias.unam.mx>
2023-06-02 01:56:26 -0600

    NJ casi listo (sólo es un caso y limpiar códigoooooooo) <3

--- src/trees.py
+++ src/trees.py
@@ -1,9 +1,10 @@
+import os
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def nj(dmatrix, g, taxas):
+def nj(dmatrix, g, taxas, nombre):
     n = len(taxas)
     if n > 2:
         # PASO 1
@@ -12,6 +13,7 @@ def nj(dmatrix, g, taxas):
         for i in range(n):
             for j in range(n):
                 r[i] += dmatrix[i][j]
+
         # 'Neighborliness' xp
         # estamos desperdiciando algo de espacio y tiempo xd    
         m = np.zeros((n,n), dtype=float)
@@ -19,14 +21,15 @@ def nj(dmatrix, g, taxas):
             for j in range(n):
                 if i != j : m[i][j] = (n-2) * dmatrix[i][j] - r[i] - r[j]
                 # Si i==j se queda en 0'ros
-    
         # Taxas a unir:
         minimo = np.unravel_index(np.argmin(m, axis=None), m.shape)
         i = minimo[0]
         j = minimo[1]
-
         taxa_i = taxas.pop(i)
-        taxa_j = taxas.pop(j)
+        if i < j:
+            taxa_j = taxas.pop(j-1)
+        else:
+            taxa_j = taxas.pop(j)
 
         nuevo_vertice = taxa_i + taxa_j 
         taxas.append(nuevo_vertice)
@@ -46,26 +49,21 @@ def nj(dmatrix, g, taxas):
 
         nuevas_distancias.append(0)
 
-        dmatrix1 = np.delete(dmatrix, j, 0)
-        dmatrix1 = np.delete(dmatrix1, j, 1)
-        dmatrix1 = np.delete(dmatrix1, i-1, 1)
-        dmatrix1 = np.delete(dmatrix1, i-1, 0)
-
+        # aquí creo q me hace falta revisar índices para ver de cuál axis borro primero xc
+        dmatrix1 = np.delete(dmatrix, [i,j], 1)
+        dmatrix1 = np.delete(dmatrix1, [i,j], 0)
         n_nueva = len(taxas)
 
         for columna in range(n_nueva-1):
             for fila in range(n_nueva-1):
                 dist_update[columna][fila] = dmatrix1[columna][fila]
         
-        print(dist_update)
-        print(nuevas_distancias)
         dist_update[:,-1] = nuevas_distancias
         dist_update[-1,:] = nuevas_distancias
-
         # Paso 3
         # Calculamos la branch length de ti a V y de tj a V 
-        Vi = (.5 * m[i][j]) + ((2*n -2) * (r[i] - r[j]))
-        Vj = (.5 * m[i][j]) + ((2*n -2) * (r[j] - r[i]))
+        Vi = (.5 * dmatrix[i][j]) + (1/(2*(n - 2)) * (r[i] - r[j]))
+        Vj = (.5 * dmatrix[i][j]) + (1/(2*(n - 2)) * (r[j] - r[i]))
         # Los quitamos de la estrella
         if(g.has_edge(0, taxa_i)) : g.remove_edge(0, taxa_i)
         if(g.has_edge(0, taxa_j)) : g.remove_edge(0, taxa_j)
@@ -74,34 +72,29 @@ def nj(dmatrix, g, taxas):
         g.add_edge(nuevo_vertice, taxa_j, weight=Vj)
 
         # Seguimos calculandoooooo....
-        nj(dist_update, g, taxas)
+        nj(dist_update, g, taxas, nombre)
     # Paso 4
     elif n == 2:
         # Unimos los dos que quedan, by a branch length d(ti,tj)
         g.add_edge(taxas[0], taxas[1], weight=dmatrix[0,1])
-
-        pos = nx.spring_layout(g, seed=7)  # positions for all nodes - seed for reproducibility
-        # nodes
-        nx.draw_networkx_nodes(g, pos, node_size=700)
-
-        # edges
-        nx.draw_networkx_edges(g, pos, g.edges, width=6)
-        # node labels
-        nx.draw_networkx_labels(g, pos, font_size=20, font_family="sans-serif")
-        # edge weight labels
-        edge_labels = nx.get_edge_attributes(g, "weight")
-        nx.draw_networkx_edge_labels(g, pos, edge_labels)
-        plt.savefig('plotgraph.png', dpi=300, bbox_inches='tight')
+        g.remove_node(0)
+        plt.figure()    
+        pos = nx.planar_layout(g)
+        weight_labels = nx.get_edge_attributes(g,'weight')
+        nx.draw(g,pos,font_color = 'white', node_shape = 's', with_labels = True,)
+        nx.draw_networkx_edge_labels(G,pos,edge_labels=weight_labels)
+        plt.savefig(os.getcwd() + '/graphs/' + nombre + '.png', dpi=300, bbox_inches='tight')
         #return g De hecho no la quiero devolver xp Vamos a ver si puede crear un png desde acá....
     else:
         raise Exception("Algo salió mal. :-) <3")
 
 
-distance_matrix = np.array([[0, 5, 9, 9],
-                            [5, 0, 10, 10],
-                            [9, 10, 0, 8],
-                            [9, 10, 8, 0]])
-t = ['a', 'b', 'c', 'd']
+distance_matrix = np.array([[0, 5, 9, 9, 8],
+                            [5, 0, 10, 10, 9],
+                            [9, 10, 0, 8, 7],
+                            [9, 10, 8, 0, 3],
+                            [8, 9, 7, 3, 0]])
+t = ['a', 'b', 'c', 'd', 'e']
 # Formando la estreeellaaaa.
 G = nx.Graph()
 G.add_node(0)
@@ -109,4 +102,4 @@ for taxa in range(len(t)):
     G.add_node(t[taxa])
     G.add_edge(0, t[taxa])
     
-nj(distance_matrix, G, t)
+nj(distance_matrix, G, t, "prueba")
